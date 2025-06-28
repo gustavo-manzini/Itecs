7c2818d78b003f15c612fa55123d7c9273173a4c
Gustavo Manzini <tavitooo14@icloud.com>
2025-06-27 22:12:40 -0300

    primer version del navbar

---
+++ src/Componentes/Navbar.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+
+import { FiSearch, FiUser, FiShoppingBag, FiMenu, FiX } from 'react-icons/fi';
+
+function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <nav className="navbar">
+      {/* Logo */}
+      <div className="navbar-logo">
+        <span className="logo-it">It</span>
+        <span className="logo-itecs">itecs</span>
+      </div>
+
+      {/* Menú Hamburguesa (visible en móvil) */}
+      <div className="navbar-menu-icon" onClick={() => setMenuOpen(!menuOpen)}>
+        {menuOpen ? <FiX /> : <FiMenu />}
+      </div>
+
+      {/* Links */}
+      <ul className={`navbar-links ${menuOpen ? 'active' : ''}`}>
+        <li>Inicio</li>
+        <li>Servicio Técnico</li>
+        <li>Tienda</li>
+        <li>Nosotros</li>
+        <li>Contacto</li>
+      </ul>
+
+      {/* Íconos */}
+      <div className="navbar-icons">
+        <FiSearch className="icon" />
+        <FiUser className="icon" />
+        <FiShoppingBag className="icon" />
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
