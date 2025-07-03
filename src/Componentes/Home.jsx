341a77d560b8362bdd739891b7eb7ce0d65ade86
Gustavo Manzini <tavitooo14@icloud.com>
2025-07-03 11:17:30 -0300

    agregue seccion

--- src/Componentes/Home.jsx
+++ src/Componentes/Home.jsx
@@ -3,9 +3,19 @@ import "../App.css";
 
 function home() {
   return (
-    <div className="rectangulo-container">
-      <div className="rectangulo"></div>
-    </div>
+    <section className="hero-section">
+      <div>
+        <p>Tu iPhone favorito, al mejor precio</p>
+        <h1>
+          Tecnología de confianza, <br /> reacondicionada para ti
+        </h1>
+        <p>Más sostenible, más inteligente, más económico</p>
+        <button className="cta-button">Comprar ahora</button>
+      </div>
+      <div className="imagen">
+        <img src="/public/home/iphone16.webp" alt="Iphone" />
+      </div>
+    </section>
   );
 }
 
