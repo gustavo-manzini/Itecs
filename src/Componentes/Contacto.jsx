8777aa0478b87a16885299134566204a8462cefa
Gustavo Manzini <tavitooo14@icloud.com>
2025-07-29 21:36:59 -0300

    alinie el mapa con el form

--- src/Componentes/Contacto.jsx
+++ src/Componentes/Contacto.jsx
@@ -1,4 +1,3 @@
-
 const Contacto = () => {
   return (
     <div className="contact-container">
@@ -32,49 +31,45 @@ const Contacto = () => {
         </div>
       </div>
 
-      <div
-        className="map-section"
-        style={{
-          marginTop: "2rem",
-          height: "400px",
-          width: "100%",
-          borderRadius: "10px",
-          overflow: "hidden",
-          
-        }}
-      >
-        
-        <h3>¿Dónde estamos?</h3>
-        <iframe className="google-map"
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3272.125167317798!2d-56.18175832446097!3d-34.90330827338156!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x959f812820d55d41%3A0x8310a9585982fd16!2sITECS%20-%20Servicio%20T%C3%A9cnico!5e0!3m2!1ses-419!2suy!4v1753205773922!5m2!1ses-419!2suy"
-          width="100%"
-          height="100%"
-          style={{ border: 0 }}
-          allowFullScreen
-          loading="lazy"
-          referrerPolicy="no-referrer-when-downgrade"
-          title="Mapa ITECS"
-        />
-      </div>
-
-      {/* Formulario */}
-
-      <div className="form-container">
-        <h2>Envíamos tu consulta</h2>
-        <p>A la brevedad, uno de nuestros asesores responderá tu solicitud.</p>
-
-        <form>
-          <div className="input-row">
-            <input type="text" placeholder="Nombre" name="nombre" />
-            <input type="text" placeholder="Apellido" name="apellido"/>
+      <div className="center-wrapper">
+        <div className="contact-section">
+          <div className="map-section">
+            <h3>¿Dónde estamos?</h3>
+            <div className="left-box">
+              <iframe
+                className="google-map"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3272.125167317798!2d-56.18175832446097!3d-34.90330827338156!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x959f812820d55d41%3A0x8310a9585982fd16!2sITECS%20-%20Servicio%20T%C3%A9cnico!5e0!3m2!1ses-419!2suy!4v1753205773922!5m2!1ses-419!2suy"
+                width="100%"
+                height="100%"
+                style={{ border: 0 }}
+                allowFullScreen=""
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+                title="Mapa ITECS"
+              />
+            </div>
           </div>
-           <div className="input-row">
-            <input type="email" placeholder="Dirección de Email" name="email" />
-            <input type="tel" placeholder="Número de Contacto" name="contacto"/>
+          <div className="form-container">
+            <div>
+              <h2>Envíanos tu consulta</h2>
+              <p>A la brevedad, uno de nuestros asesores responderá tu solicitud.</p>
+              <div className="right-box">
+                <form>
+                  <div className="input-row">
+                    <input type="text" placeholder="Nombre" name="nombre" />
+                    <input type="text" placeholder="Apellido" name="apellido" />
+                  </div>
+                  <div className="input-row">
+                    <input type="email" placeholder="Dirección de Email" name="email" />
+                    <input type="tel" placeholder="Número de Contacto" name="contacto" />
+                  </div>
+                  <textarea placeholder="Mensaje" name="mensaje" rows="6" />
+                  <button type="submit">Enviar</button>
+                </form>
+              </div>
+            </div>
           </div>
-           <textarea placeholder="Mensaje" name="mensaje" rows="6" />
-          <button type="submit">Enviar</button>
-        </form>
+        </div>
       </div>
     </div>
   );
