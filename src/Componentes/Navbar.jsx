0965c979c8106deb63f0c67573fc6fdaa3d64866
Gustavo-Manzini <gustavo.manzini@anima.edu.uy>
2025-08-06 10:16:42 -0300

    corregi foto del logo

--- src/Componentes/Navbar.jsx
+++ src/Componentes/Navbar.jsx
@@ -10,7 +10,7 @@ function Navbar() {
       <div className="navbar-container">
         <Link to="/" className="navbar-logo">
           <img
-            src="./public/logo/logo-iytecs.png"
+            src="./public/logo/logo-itecs.png"
             alt="Logo "
             className="logo-itecs"
           />
